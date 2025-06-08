1e64ac42ac2bb325478f41fe2afb2281ceac9a6e
Z0TYER <zxcemxcombos789@gmail.com>
2025-06-08 22:44:40 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,5 +2,5 @@ API_ID = 21052090
 API_HASH = "41f001102c8fe7411baf00423df11bbb"
 BOT_TOKEN = "7230167851:AAFGoVG8Xenx-F9ffPZTOY23EZ3jcWX-Ys4"
 BOT_USERNAME = "Akekwkh_bot"  # without @
-MONGO_URI = "mongodb://localhost:27017"
+MONGO_URI = "mongodb+srv://rtionoya:6OWHg-MGzvu-Ewpkh8@cluster0.dpaer.mongodb.net/?retryWrites=true&w=majority"
 DB_NAME = "tictactoe"
